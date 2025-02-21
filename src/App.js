ca444a78ce0d08932898fba72fcba319e57feced
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-02-21 23:43:59 +0100

    ajouter route path in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import Artisan from "./pages/Artisan";
+import Circuits from "./pages/Circuits";
+import Contact from "./pages/Contact";
+import Reservation from "./pages/Reservation";
+
+
+
 function App() {
   return (
     <>
-      <h1 className="title">Hello</h1>
-      <h2>Bon soir </h2>
-      <h3>Cv</h3>
+    <Routes>
+      <Route path="/" element={<Accueil/>} />
+      <Route path="/Artisan" element={<Artisan/>} />
+      <Route path="/Circuits" element={<Circuits/>} />
+      <Route path="/Contact" element={<Contact/>} />
+      <Route path="/Reservation" element={<Reservation/>} />
+
+
+
+
+    </Routes>
       <h1 className="title">My Name Amine</h1>
     </>
   );
