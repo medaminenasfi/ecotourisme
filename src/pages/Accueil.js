432b563cb056ac30dbb453ed938aa9814ee1af4e
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-02-24 21:02:13 +0100

    test

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -3,17 +3,16 @@ import Navbar from "../Components/navbar";
 import "./accueil.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../Components/footer";
+
 const Accueil = () => {
   console.log("Rendering Accueil page");
   return (
     <>
       <Navbar />
       <main>
-      <h1>Welcome to Accueil</h1>
-
-
+        <h1>Welcome to Accueil</h1>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 };
