3feb34817a7c06767e44eb36cc18c107e3053377
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-02-25 21:43:43 +0100

    developer la page de artisan et circuit pas complet

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,16 +1,75 @@
 import React from "react";
 import Navbar from "../Components/navbar";
+import "./Reservation.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../Components/footer";
-const Reservation = () => {
+import backgroundImage from "../assest/Accueil.jpg";
+
+
+
+
+
+const Accueil = () => {
+  console.log("Rendering Accueil page");
   return (
     <>
       <Navbar />
+      <main>
+        <section
+          className="d-flex align-items-center justify-content-center text-white"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            height: "100vh",
+            width: "100%",
+          }}
+        >
+          <div className="overlay"></div>
+          <div className="content text-center">
+            <h1>
+            Votre Aventure Commence Ici
+            </h1>
+            <p className="lead">
+            La nature ne fait rien en vain, et chaque paysage est une invitation à l'aventure
+            </p>
+          </div>
+        
+        </section>
+        <section className="bg-black text-white p-5 shadow-lg">
+          <h1 classname="hell">Les meilleures destinations</h1>
+          <center>
+            {" "}
+            <p>
+              Partez à la découverte de paysages incroyables, des déserts aux
+              plages superbes.
+            </p>{" "}
+          </center>
 
-      <h1>Welcome to Reservation</h1>
-      <Footer/>
-
+          
+        
+          
+        </section>
+        <section className="bg-black text-white p-5 shadow-lg">
+          <center>
+            <section className="image-section d-flex justify-content-center align-items-center">
+              <div className="main-container">
+                <div className="mask-group" />
+                <span className="explore-tunisia">
+                  Explorez
+                  <br />
+                  la Tunisie autrement
+                </span>
+                <div className="frame-1" />
+              </div>{" "}
+            </section>
+          </center>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
 
-export default Reservation;
+export default Accueil;
