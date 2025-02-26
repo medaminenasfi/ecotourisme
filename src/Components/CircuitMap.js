bb6ffedc17fc508a30b262fb38a269e572d255cc
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-02-27 00:20:48 +0100

    cirr updatz

--- src/Components/CircuitMap.js
+++ src/Components/CircuitMap.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import { MapContainer, TileLayer, Marker, Popup, Polyline, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { Button } from 'react-bootstrap';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 // Circuit Coordinates
 const tozeurCircuitCoordinates = [
@@ -21,48 +23,22 @@ const pointsOfInterest = [
   { name: "Le Palmier Restaurant", position: [33.9280, 8.1480], type: "restaurant" },
 ];
 
-// Leaflet Icons
-const startIcon = new L.Icon({
-  iconUrl: "/icons/start-marker.png",
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-  popupAnchor: [0, -20],
-});
+// Leaflet Default Icons
+const createIcon = (size = [40, 40]) => 
+  new L.Icon({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+    iconSize: size,
+    iconAnchor: [size[0] / 2, size[1]],
+    popupAnchor: [0, -size[1] / 2],
+  });
 
-const finishIcon = new L.Icon({
-  iconUrl: "/icons/finish-marker.png",
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-  popupAnchor: [0, -20],
-});
-
-const arrowIcon = new L.Icon({
-  iconUrl: "/icons/arrow-marker.png",
-  iconSize: [25, 25],
-  iconAnchor: [12, 25],
-  popupAnchor: [0, -12],
-});
-
-const restaurantIcon = new L.Icon({
-  iconUrl: "/icons/restaurant-marker.png",
-  iconSize: [30, 30],
-  iconAnchor: [15, 30],
-  popupAnchor: [0, -15],
-});
-
-const guesthouseIcon = new L.Icon({
-  iconUrl: "/icons/hotel-marker.png",
-  iconSize: [30, 30],
-  iconAnchor: [15, 30],
-  popupAnchor: [0, -15],
-});
-
-const userLocationIcon = new L.Icon({
-  iconUrl: "/icons/user-marker.png",
-  iconSize: [30, 30],
-  iconAnchor: [15, 30],
-  popupAnchor: [0, -15],
-});
+const startIcon = createIcon();
+const finishIcon = createIcon();
+const arrowIcon = createIcon([25, 25]);
+const restaurantIcon = createIcon([30, 30]);
+const guesthouseIcon = createIcon([30, 30]);
+const userLocationIcon = createIcon([30, 30]);
 
 // Handle Clicks on the Map
 const ClickHandler = ({ setClickedPosition }) => {
