47ded802468fc3ddb28634207eab9b3c705d7aa9
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-02-26 17:07:30 +0100

    add calendar

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -4,11 +4,13 @@ import "./Reservation.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../Components/footer";
 import backgroundImage from "../assest/Accueil.jpg";
-
-
-
-
-
+import Container from "react-bootstrap/Container";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { Box } from "@mui/material";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const Accueil = () => {
   console.log("Rendering Accueil page");
   return (
@@ -28,28 +30,42 @@ const Accueil = () => {
         >
           <div className="overlay"></div>
           <div className="content text-center">
-            <h1>
-            Votre Aventure Commence Ici
-            </h1>
+            <h1>Votre Aventure Commence Ici</h1>
             <p className="lead">
-            La nature ne fait rien en vain, et chaque paysage est une invitation à l'aventure
+              La nature ne fait rien en vain, et chaque paysage est une
+              invitation à l'aventure
             </p>
           </div>
-        
         </section>
-        <section className="bg-black text-white p-5 shadow-lg">
+        <section className="bg-black   text-white p-5 shadow-lg">
           <h1 classname="hell">Les meilleures destinations</h1>
           <center>
-            {" "}
             <p>
               Partez à la découverte de paysages incroyables, des déserts aux
               plages superbes.
             </p>{" "}
           </center>
+          <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>
+        <center>
+        <Box display="flex" justifyContent="center" mt={3}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <StaticDatePicker orientation="landscape" />
+            </LocalizationProvider>
+          </Box>
+          </center>
+        
+        
+        </Col>
+      </Row>
+    
+    </Container>
+
 
           
-        
-          
+
         </section>
         <section className="bg-black text-white p-5 shadow-lg">
           <center>
