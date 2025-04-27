fa79a1d6495cbd4bfe500e4f666113caea214bd2
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-04-27 21:50:16 +0100

    2323

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -17,9 +17,91 @@ const cities = [
   { name: "Tunis", coords: [36.8065, 10.1815] },
   { name: "Sousse", coords: [35.8254, 10.6369] },
   { name: "Sfax", coords: [34.7406, 10.7603] },
-  {   name: 'Ariana',   coords: [36.8665, 10.1647], 
+  {   name: 'Ariana',   coords: [36.8665, 10.1647], },
+  { 
+    name: 'Ben Arous', 
+    coords: [36.7435, 10.2317], 
   },
-
+  { 
+    name: 'Manouba', 
+    coords: [36.8083, 9.9991], 
+  },
+  { 
+    name: 'Nabeul', 
+    coords: [36.451, 10.7361], 
+  },
+  { 
+    name: 'Bizerte', 
+    coords: [37.2744, 9.8739], 
+  },
+  { 
+    name: 'Beja', 
+    coords: [36.733, 9.1843], 
+  },
+  { 
+    name: 'Jendouba', 
+    coords: [36.5011, 8.7802], 
+  },
+  { 
+    name: 'Kef', 
+    coords: [36.1675, 8.704], 
+  },
+  { 
+    name: 'Siliana', 
+    coords: [36.088, 9.3746], 
+  },
+  { 
+    name: 'Zaghouan', 
+    coords: [36.4021, 10.1447], 
+  },
+  { 
+    name: 'Monastir', 
+    coords: [35.7643, 10.8113], 
+  },
+  { 
+    name: 'Mahdia', 
+    coords: [35.5047, 11.0622], 
+  },
+  { 
+    name: 'Sfax', 
+    coords: [34.7391, 10.7593], 
+  },
+  { 
+    name: 'Kairouan', 
+    coords: [35.6781, 10.0963], 
+  },
+  { 
+    name: 'Kasserine', 
+    coords: [35.1676, 8.8368], 
+  },
+  { 
+    name: 'Sidi Bouzid', 
+    coords: [35.0382, 9.4858], 
+  },
+  { 
+    name: 'Gabes', 
+    coords: [33.8815, 10.0982], 
+  },
+  { 
+    name: 'Medenine', 
+    coords: [33.3549, 10.5055], 
+  },
+  { 
+    name: 'Tataouine', 
+    coords: [32.929, 10.4518], 
+  },
+  { 
+    name: 'Tozeur', 
+    coords: [33.9197, 8.1335], 
+  },
+  { 
+    name: 'Kebili', 
+    coords: [33.7076, 8.9715], 
+  },
+  { 
+    name: 'Gafsa', 
+    coords: [34.425, 8.7842], 
+  }
 ]
 
 function MapController({ center, zoom }) {
